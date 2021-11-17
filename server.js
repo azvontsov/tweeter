3877660a5f46f8468e9c1c76a0a68e922a782aa4
Anton <anton@Polinas-MacBook-Air.local>
2021-11-16 20:33:59 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+// require dependecies
+const express = require('express');
+// initialize the express app
+const app = express();
+
+// configure settings
+const PORT = 3000;
+
+// connect to and config mongodb
+
+// mounting middleware
+
+// mount routes
+
+
+
+// tell the app to listen
+app.listen(PORT, () => {
+    console.log(`listening port on 3000:${PORT}`);
+})
